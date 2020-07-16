d09bd487784a3febc8260576c6adbc3a2d7ea440
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-16 04:54:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,7 @@
-def prime?(number)
-  if prime? == 2
-    return "true"
-    end
-  end
+def prime?(num)
+  if num <= 1
+    return false
+  else (2..num/2).each{|n|num % n == 0}
+    return true
+ end
+end
